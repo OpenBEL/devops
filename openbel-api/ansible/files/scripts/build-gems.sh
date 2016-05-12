42730b0cf8a3c00b2edb8564acaa4db046ddda16
William Hayes <william.s.hayes@gmail.com>
2016-05-12 12:05:14 -0400

    Working OpenBEL API dev environment
    This is set up to use the next (dev) branches of several of the Ruby OpenBEL libraries.

---
+++ openbel-api/ansible/files/scripts/build-gems.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+export PATH=/opt/jruby/bin:$GEM_HOME/bin:$PATH
+export JRUBY_HOME=/opt/jruby
+export GEM_HOME=/opt/jruby/gems
+export GEM_PATH=/opt/jruby/gems
+
+cd /openbel/repos/bel_parser.rb
+git pull
+gem build .gemspec-java
+gem install *.gem
+
+cd /openbel/repos/bel.rb
+git pull
+gem build .gemspec
+gem install *.gem
+
+cd /openbel/repos/rdf-jena
+git pull
+gem build rdf-jena.gemspec
+gem install *.gem
+
+cd /openbel/repos/bel.rb-rdf-jena
+git pull
+gem build .gemspec
+gem install *.gem
+
+cd /openbel/repos/bel.rb-search-sqlite
+git pull
+gem build .gemspec-java
+gem install *.gem
+
+
+
+cd /openbel/api
+git pull
+gem build .gemspec
+gem install *.gem
--- openbel-api/ansible/files/scripts/install-jruby.sh
+++ openbel-api/ansible/files/scripts/install-jruby.sh
@@ -4,7 +4,8 @@ cd /opt/rubies
 wget https://s3.amazonaws.com/jruby.org/downloads/9.0.5.0/jruby-bin-9.0.5.0.tar.gz
 tar xzf jruby-bin-9.0.5.0.tar.gz
 
-ln -s /opt/rubies/jruby-bin-9.0.5.0 /opt/jruby
+ln -s /opt/rubies/jruby-9.0.5.0 /opt/jruby
+ln -s /opt/rubies/jruby-9.0.5.0/bin/jruby /opt/rubies/jruby-9.0.5.0/bin/ruby
 
 rm jruby-bin-9.0.5.0.tar.gz
 
